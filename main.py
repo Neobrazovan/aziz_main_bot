7ae55f7d41acaed6189df43a4010ba7c8a9cb40d
Neobrazovan <abdullohpupsik@gmail.com>
2025-07-23 18:26:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from aiogram import Bot, Dispatcher, types, executor
+import logging
+import os
+
+API_TOKEN = os.getenv("BOT_TOKEN")
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def start_handler(message: types.Message):
+    await message.answer("Привет! Это основной бот.")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
